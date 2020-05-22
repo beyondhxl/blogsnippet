da71b53e84c0fa00ca20d29740a80e1944ea64b2
beyondhxl <459898432@qq.com>
2020-05-22 11:07:28 +0800

    [配置] 增加gitignore忽视文件

--- 数据结构与算法/复杂链表的复制/complexlist.go
+++ 数据结构与算法/复杂链表的复制/complexlist.go
@@ -26,7 +26,6 @@ func CloneComplexList(head *ComplexListNode) *ComplexListNode {
 			}
 			m[cur].Next = m[cur.Next]
 		}
-
 		if cur.Sibling != nil {
 			if _, ok := m[cur.Sibling]; !ok {
 				m[cur.Sibling] = &ComplexListNode{cur.Sibling.Val, nil, nil}
@@ -58,5 +57,4 @@ func main() {
 	fmt.Println()
 	fmt.Println("复制后 --------- ")
 	printList(CloneComplexList(l1))
-	fmt.Println("")
 }
