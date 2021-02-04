54f7a2d332ed17fc5988f8c1abe479b9516ae44b
beyondhxl <459898432@qq.com>
2021-02-04 16:18:54 +0800

    [go应用] web + cache

---
+++ gocache/cache/cache.go
@@ -0,0 +1,37 @@
+package cache
+
+import (
+	"cache/lru"
+	"sync"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.CaChe
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil) // 延迟初始化(Lazy Initialization)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+
+	return
+}
---
+++ gocache/go.mod
@@ -0,0 +1,8 @@
+module example
+
+go 1.14
+
+require cache v0.0.0
+
+replace cache => ./cache
+
