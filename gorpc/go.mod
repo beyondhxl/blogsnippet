b6055741d75704fa42a131b2393f61361fb2ff6c
beyondhxl <459898432@qq.com>
2021-02-23 21:40:52 +0800

    [go应用] rpc

--- gorpc/go.mod
+++ gorpc/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575 // indirect
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/protobuf v1.4.3 // indirect
+	google.golang.org/protobuf v1.23.0
 )
--- gorpc/main/main.go
+++ gorpc/main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"gorpc"
 	"log"
 	"net"
@@ -9,7 +8,23 @@ import (
 	"time"
 )
 
+type Foo int
+
+type Args struct {
+	Num1, Num2 int
+}
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := gorpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
+
 	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
@@ -58,14 +73,21 @@ func main() {
 		go func(i int) {
 			defer wg.Done()
 
-			args := fmt.Sprintf("gorpc req %d", i)
+			//args := fmt.Sprintf("gorpc req %d", i)
+			//
+			//var reply string
+			//if err := client.Call("Foo.Sum", args, &reply); err != nil {
+			//	log.Fatal("call Foo.Sum error:", err)
+			//}
+			//
+			//log.Println("reply:", reply)
 
-			var reply string
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
-
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 
