72958311921f76aafe042d8c0174c7ebab50b3e9
beyondhxl <459898432@qq.com>
2021-02-24 15:04:05 +0800

    [go应用] rpc

--- gorpc/main/main.go
+++ gorpc/main/main.go
@@ -1,95 +1,58 @@
 package main
 
 import (
+	"context"
 	"gorpc"
 	"log"
 	"net"
+	"net/http"
 	"sync"
 	"time"
 )
 
 type Foo int
 
-type Args struct {
-	Num1, Num2 int
-}
+type Args struct{ Num1, Num2 int }
 
 func (f Foo) Sum(args Args, reply *int) error {
 	*reply = args.Num1 + args.Num2
 	return nil
 }
 
-func startServer(addr chan string) {
+func startServer(addrCh chan string) {
 	var foo Foo
-	if err := gorpc.Register(&foo); err != nil {
-		log.Fatal("register error:", err)
-	}
-
-	// pick a free port
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("network error:", err)
-	}
-	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String()
-	gorpc.Accept(l)
+	l, _ := net.Listen("tcp", ":9999")
+	_ = gorpc.Register(&foo)
+	gorpc.HandleHTTP()
+	addrCh <- l.Addr().String()
+	_ = http.Serve(l, nil)
 }
 
-func main() {
-	log.SetFlags(0)
-	addr := make(chan string)
-	go startServer(addr)
-
-	// in fact, following code is like a simple geerpc client
-	//conn, _ := net.Dial("tcp", <-addr)
-	//defer func() { _ = conn.Close() }()
-	//
-	//time.Sleep(time.Second)
-	//// send options
-	//_ = json.NewEncoder(conn).Encode(gorpc.DefaultOption)
-	//cc := codec.NewGobCodec(conn)
-	//// send request & receive response
-	//for i := 0; i < 5; i++ {
-	//	h := &codec.Header{
-	//		ServiceMethod: "Foo.Sum",
-	//		Seq:           uint64(i),
-	//	}
-	//	_ = cc.Write(h, fmt.Sprintf("gorpc req %d", h.Seq))
-	//	_ = cc.ReadHeader(h)
-	//	var reply string
-	//	_ = cc.ReadBody(&reply)
-	//	log.Println("reply:", reply)
-	//}
-
-	client, _ := gorpc.Dial("tcp", <-addr)
+func call(addrCh chan string) {
+	client, _ := gorpc.DialHTTP("tcp", <-addrCh)
 	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-
+	// send request & receive response
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
-
 		go func(i int) {
 			defer wg.Done()
-
-			//args := fmt.Sprintf("gorpc req %d", i)
-			//
-			//var reply string
-			//if err := client.Call("Foo.Sum", args, &reply); err != nil {
-			//	log.Fatal("call Foo.Sum error:", err)
-			//}
-			//
-			//log.Println("reply:", reply)
-
 			args := &Args{Num1: i, Num2: i * i}
 			var reply int
-			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
 			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
-
 	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
 }
